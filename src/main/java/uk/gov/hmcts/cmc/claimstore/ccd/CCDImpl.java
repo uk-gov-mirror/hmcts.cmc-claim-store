a639f5731d28a6e58d0bb82decdcc4c0ec084614
dharmendra kumar <dharmendra.kumar@hmcts.net>
2018-06-01 07:57:11 +0100

    add search crieteria

--- src/main/java/uk/gov/hmcts/cmc/claimstore/ccd/CCDImpl.java
+++ src/main/java/uk/gov/hmcts/cmc/claimstore/ccd/CCDImpl.java
@@ -1,15 +1,15 @@
 package uk.gov.hmcts.cmc.claimstore.ccd;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ccd.ICCDApplication;
 import uk.gov.hmcts.ccd.domain.model.aggregated.CaseViewTrigger;
 import uk.gov.hmcts.ccd.domain.model.aggregated.ProfileCaseState;
+import uk.gov.hmcts.cmc.claimstore.exceptions.NotFoundException;
 import uk.gov.hmcts.cmc.claimstore.repositories.ClaimRepository;
 import uk.gov.hmcts.cmc.domain.models.Claim;
-import uk.gov.hmcts.cmc.domain.models.sampledata.SampleClaim;
 
 import java.util.List;
 import java.util.Map;
@@ -26,12 +26,32 @@ public class CCDImpl implements ICCDApplication<Claim> {
     // Allows CCD to fetch cases from the service.
     @Override
     public List<Claim> getCases(Map<String, String> searchCriteria) {
+        if (searchCriteria != null && !searchCriteria.isEmpty()) {
+            for (String key : searchCriteria.keySet()) {
+                switch (key) {
+                    case "id":
+                        return ImmutableList.of(repository.getById(Long.valueOf(searchCriteria.get("id")))
+                            .orElseThrow(() -> new NotFoundException("")));
+                    case "externalId":
+                        return ImmutableList.of(repository.getById(Long.valueOf(searchCriteria.get("externalId")))
+                            .orElseThrow(() -> new NotFoundException("")));
+                    case "submitterId":
+                        return ImmutableList.of(repository.getById(Long.valueOf(searchCriteria.get("submitterId")))
+                            .orElseThrow(() -> new NotFoundException("")));
+                    case "referenceNumber":
+                        return ImmutableList.of(repository.getById(Long.valueOf(searchCriteria.get("submitterId")))
+                            .orElseThrow(() -> new NotFoundException("")));
+
+
+                }
+            }
+        }
         return repository.findAll();
     }
 
     // Called by CCD when a caseworker creates a case.
     @Override
-    public void saveCase(Claim c) {
+    public void saveCase(Claim claim) {
         //
     }
 
