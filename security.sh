904c18863c96ec44c5e9d042ee403fe9fef0d6fd
Mark Ryan <mark.ryan@hmcts.net>
2019-07-24 14:03:42 +0100

    cp chown from citizen

--- security.sh
+++ security.sh
@@ -3,5 +3,7 @@ echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
+chown -R $(id -u):$(id -u) /zap/api-report.html
 cp /zap/api-report.html functional-output/
 zap-cli -p 1001 alerts -l High
