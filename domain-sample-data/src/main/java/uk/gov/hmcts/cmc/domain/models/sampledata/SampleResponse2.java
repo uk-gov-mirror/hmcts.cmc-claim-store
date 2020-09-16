4e12350783538344b57f7aa414501626a1ae7981
sunilkumar-cts <sunil.kumar@hmcts.net>
2020-09-16 11:39:03 +0100

    Fix/nightly tests (#1921)
    * Roc 8343 nightly functional tests (#1919)
    
    * Update values.yaml
    
    Enabled CTSC flag for functional tests to pickup
    
    * Bumping chart version
    
    * Update RespondToClaimTest.java
    
    Added Sysouts
    
    * Update RespondToClaimTest.java
    
    * addde sysouts
    
    * ROC-8343 Nightly Functional tests fix
    
    * ROC-8343 Nightly Functional tests fix
    
    * ROC-8343 Nightly Functional tests fix
    
    * ROC-8343 Nightly Functional tests fix
    
    * ROC-8343 Nightly Functional tests fix
    
    Co-authored-by: hmcts-jenkins-cmc <62421507+hmcts-jenkins-cmc[bot]@users.noreply.github.com>
    
    * nightly functional test
    
    * nightly test
    
    Co-authored-by: yogendraU <66823759+yogendraU@users.noreply.github.com>
    Co-authored-by: hmcts-jenkins-cmc <62421507+hmcts-jenkins-cmc[bot]@users.noreply.github.com>

--- domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/SampleResponse2.java
+++ domain-sample-data/src/main/java/uk/gov/hmcts/cmc/domain/models/sampledata/SampleResponse2.java
@@ -5,7 +5,15 @@ import uk.gov.hmcts.cmc.domain.models.directionsquestionnaire.DirectionsQuestion
 import uk.gov.hmcts.cmc.domain.models.evidence.DefendantEvidence;
 import uk.gov.hmcts.cmc.domain.models.legalrep.StatementOfTruth;
 import uk.gov.hmcts.cmc.domain.models.party.Party;
-import uk.gov.hmcts.cmc.domain.models.response.*;
+import uk.gov.hmcts.cmc.domain.models.response.DefenceType;
+import uk.gov.hmcts.cmc.domain.models.response.DefendantTimeline;
+import uk.gov.hmcts.cmc.domain.models.response.FullAdmissionResponse;
+import uk.gov.hmcts.cmc.domain.models.response.FullDefenceResponse;
+import uk.gov.hmcts.cmc.domain.models.response.PartAdmissionResponse;
+import uk.gov.hmcts.cmc.domain.models.response.PaymentIntention;
+import uk.gov.hmcts.cmc.domain.models.response.Response;
+import uk.gov.hmcts.cmc.domain.models.response.ResponseMethod;
+import uk.gov.hmcts.cmc.domain.models.response.YesNoOption;
 import uk.gov.hmcts.cmc.domain.models.sampledata.response.SamplePaymentIntention;
 import uk.gov.hmcts.cmc.domain.models.sampledata.statementofmeans.SampleStatementOfMeans;
 
@@ -127,6 +135,7 @@ public abstract class SampleResponse2<T extends SampleResponse2<T>> {
                 .timeline(SampleDefendantTimeline.validDefaults())
                 .evidence(SampleDefendantEvidence.validDefaults())
                 .statementOfMeans(SampleStatementOfMeans.builder().build())
+                .responseMethod(ResponseMethod.DIGITAL)
                 .build();
         }
 
@@ -289,7 +298,7 @@ public abstract class SampleResponse2<T extends SampleResponse2<T>> {
                 freeMediationOption, mediationPhoneNumber, mediationContactPerson,
                 moreTimeNeededOption, defendantDetails, statementOfTruth,
                 defenceType, defence, paymentDeclaration, timeline, evidence, directionsQuestionnaire,
-                ResponseMethod.DIGITAL
+                null
             );
         }
     }
